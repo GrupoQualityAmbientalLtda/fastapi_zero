42501584aef295655a648a8ec47b27b32cef2c2b
Grupo Quality Ambiental <ti@grupoqualityambiental.com.br>
2025-08-29 13:00:30 -0300

    estudos fastapi

--- src/fastapi_zero/app.py
+++ src/fastapi_zero/app.py
@@ -1,8 +1,13 @@
+from http import HTTPStatus
+
 from fastapi import FastAPI
 
-app = FastAPI()
+from fastapi_zero.schemas import Message
+
+app = FastAPI(title='API da biblioteca')
 
 
-@app.get('/')
+@app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
-    return {'message': 'Olá Mundo!'}
+    return {'message': 'oi mundo'}
+    # return {'message': 'Olá Mundo!'}
