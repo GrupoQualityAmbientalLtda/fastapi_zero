ebfefec4b31082f0ad8ff648ed72fe4b67f84aa2
Grupo Quality Ambiental <ti@grupoqualityambiental.com.br>
2025-09-02 16:20:51 -0300

    testes

--- src/fastapi_zero/app.py
+++ src/fastapi_zero/app.py
@@ -2,12 +2,27 @@ from http import HTTPStatus
 
 from fastapi import FastAPI
 
-from fastapi_zero.schemas import Message
+from fastapi_zero.schemas import Message, UserDB, UserPublic, UserSchema
 
 app = FastAPI(title='API da biblioteca')
+
+database = []
 
 
 @app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
     return {'message': 'oi mundo'}
     # return {'message': 'Olá Mundo!'}
+
+
+@app.post('/users/', status_code=HTTPStatus.CREATED, response_model=UserPublic)
+def create_user(user: UserSchema):
+    user_with_id = UserDB(
+        username=user.username,
+        email=user.email,
+        password=user.password,
+        id=len(database) + 1,
+    )
+
+    database.append(user_with_id)
+    return user_with_id
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,8 +1,14 @@
 from http import HTTPStatus
 
+import pytest
 from fastapi.testclient import TestClient
 
 from src.fastapi_zero.app import app
+
+
+@pytest.fixture
+def client():
+    return TestClient(app)
 
 
 def test_root_retornar_e_ok_ola_mundo():
@@ -11,4 +17,24 @@ def test_root_retornar_e_ok_ola_mundo():
     response = client.get('/')  # ACT, fase de ação
 
     assert response.status_code == HTTPStatus.OK  # Assert (Afirmação)
-    assert response.json() == {'message': 'Olá Mundo!'}
+    assert response.json() == {'message': 'oi mundo'}
+
+
+def test_create_user():
+    client = TestClient(app)
+
+    response = client.post(
+        '/users/',
+        json={
+            'username': 'LucasLopes',
+            'email': 'lucas@email.com',
+            'password': 'secret',
+        },
+    )
+
+    assert response.status_code == HTTPStatus.CREATED
+    assert response.json() == {
+        'id': 1,
+        'email': 'lucas@email.com',
+        'username': 'LucasLopes',
+    }
